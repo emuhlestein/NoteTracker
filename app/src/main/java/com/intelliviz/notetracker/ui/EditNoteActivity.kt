48997c5aec54879c323c43f1c1e8aa7941a07c8e
Ed Muhlestein <emuhlestein@yahoo.com>
2018-11-19 12:25:33 -0600

    Add buttons to edit activity

--- app/src/main/java/com/intelliviz/notetracker/ui/EditNoteActivity.kt
+++ app/src/main/java/com/intelliviz/notetracker/ui/EditNoteActivity.kt
@@ -2,7 +2,6 @@ package com.intelliviz.notetracker.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import com.intelliviz.notetracker.R
 
 class EditNoteActivity : AppCompatActivity() {
@@ -10,10 +9,11 @@ class EditNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_note)
+       // setSupportActionBar(edit_note_toolbar)
 
-        val actionBar = findViewById<Toolbar>(R.id.edit_note_toolbar)
-
-        setSupportActionBar(actionBar)
+//        val actionBar = findViewById<Toolbar>(R.id.edit_note_toolbar)
+//
+//        setSupportActionBar(actionBar)
         //val actionBar = supportActionBar
         //actionBar?.subtitle = "test"
     }
